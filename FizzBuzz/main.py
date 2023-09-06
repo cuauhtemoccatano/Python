66e6e30c68524d5ba16a29000fa5ef32e61f6de3
Cuauhtémoc Cataño De Pavía <70673042+cuauhtemoccatano@users.noreply.github.com>
2023-09-06 12:30:10 -0600

    Python Exercise

--- FizzBuzz/main.py
+++ FizzBuzz/main.py
@@ -1,5 +1,10 @@
 for lista in range(1, 101):
-    print(lista)
 
-if lista % 3 == 0:
-    print("Fizz")
+    if lista % 3 == 0 and lista % 5 == 0:
+        print("FizzBuzz")
+    elif lista % 3 == 0:
+        print("Fizz")
+    elif lista % 5 == 0:
+        print("Buzz")
+    else:
+        print(lista)
